01c9dd8339556b2b5559b725c2f5732235a54cd7
M-Caesar <43552798+M-Caesar@users.noreply.github.com>
2025-05-07 20:14:22 -0400

    template code

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("clickMeBtn").addEventListener("click", function () {
+    const msgArea = document.getElementById("messageArea");
+    msgArea.textContent = "Thanks for clicking!";
+  });
+  
